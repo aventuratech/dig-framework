060f96adc77247d1472fc0ee6cb5d313e5f741da
forrestLyman <forrestlyman@gmail.com>
2018-07-04 20:38:39 -0700

    sets up basic quickstart

--- src/dig-framework-starter/src/App.js
+++ src/dig-framework-starter/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Dashboard from "components/pages/Dashboard/Dashboard";
+import Reports from "components/pages/Reports/Reports";
+import Layout from "components/ui/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/reports" component={Reports} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
     );
   }
 }
