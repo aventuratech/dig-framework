5e0700f58ff8bc90bb647fcdac5c794df853b772
forrestLyman <forrestlyman@gmail.com>
2018-07-04 19:56:01 -0700

    exports items

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export {default as DigUi} from "dist/ui/DigUi";
+// UI Components
+export { default as DigUi } from "./dist/ui/DigUi/DigUi";
+export { default as DigUiNavItem } from "./dist/ui/DigUi/DigUiNavItem/DigUiNavItem";
+
+// Core Components
+export { default as withDig } from "./dist/core/withDig/withDig";
